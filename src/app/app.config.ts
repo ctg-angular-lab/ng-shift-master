e4c312f826917261a31396e9d0add4578f430e86
Tbars84 <camoilo.tabares.84@gmail.com>
2026-02-06 09:32:40 -0500

    core:initial scalfold  with angular 21 zoneless and functional config

---
+++ src/app/app.config.ts
@@ -0,0 +1,16 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideRouter(routes, withComponentInputBinding(),withViewTransitions()),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(withFetch(), withInterceptors([]))
+  ]
+};
