b4c359da8dae7b4fa2bb18335fd6a3398421d0f0
Tbars84 <camilo.tabares.84@gmail.com>
2026-02-11 16:37:12 -0500

    Add form validators and used it in booking form for shifts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { QrPage } from './features/qr-display/pages/qr-page/qr-page';
 import { QueueList } from '@features/queue-list/queue-list';
+import { BookingForm } from '@features/booking-form/booking-form';
 
 export const routes: Routes = [
   {path:'', component: QrPage},
-  {path:'waiting-list', component: QueueList}
+  {path:'waiting-list', component: QueueList},
+  {path:'booking-form', component: BookingForm},
 ];
